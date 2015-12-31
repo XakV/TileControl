9271132249e2faaa692198190dddc64f56b9b6c7
gr3yman <greyman@insight.rr.com>
2015-12-30 20:33:43 -0500

    big

--- TileCtrl.py
+++ TileCtrl.py
@@ -1,30 +1,30 @@
 #!/usr/env/python3
 
 import subprocess
-import Tinydb
-import Tkinter as tk
+import tinydb
+from tinydb.storages import MemoryStorage
+import tkinter as tk
+
+## who knows if this is working or not?
 
 def keypress(event):
     if event.keysym == 'Escape':
         root.destroy()
     x = event.char
     if x == "Ctrl-Z":
-        execmastr1()
+        print('c z')
     elif x == "Ctrl-X":
-        execmastr2()
+        print('c x')
     else:
         pass
 
-
-## function to get active windows and assign a role
+## function to get all active windows
 ## see man wmctrl for info on wmctrl command
     
 def buildwindb():
-    roles = ['master',  'sub1', 'sub2',  'sub3',  'sub4']
-    rolemark = 0
-    oldesk = -1
-    windodb = Tinydb('/tmp/windodb.json')
-    with subprocess.Popen(['wmctrl', '-lG'], stdout=subprocess.PIPE, universal_newlines=True) as wmctrlg:
+    windodb = tinydb.TinyDB(storage=MemoryStorage)
+    windodb.purge()
+    with subprocess.Popen(['wmctrl -lG | tr -s " "'], shell = True, stdout=subprocess.PIPE, universal_newlines=True) as wmctrlg:
         winout = wmctrlg.stdout.read().splitlines()
     for line in winout:
         winline = line.split(' ')
@@ -34,16 +34,13 @@ def buildwindb():
         ypos = winline[3]
         width = winline[4]
         height = winline[5]
-        role = roles[rolemark]
-        if dsktp == oldesk:
-            rolemark += 1
-        else:
-            rolemark = 0
-        windodb.insert({'hexid': hexid, 'desktop': dsktp, 'xposv': xpos, 'ypos': ypos, 'width': width, 'height': height, 'role': role})		
+        windodb.insert({'hexid': hexid, 'desktop': dsktp, 'xpos': xpos, 'ypos': ypos, 'width': width, 'height': height})
+        print(windodb.all())		
     return windodb
 
 ## Function to get screen dimensions and active desktop
 ## see man wmctrl for info on wmctrl command
+## this is also working
 
 def getscreen():
     with subprocess.Popen(['wmctrl', '-d'], stdout=subprocess.PIPE, universal_newlines=True) as wmctrld:
@@ -61,66 +58,34 @@ def move(winhxid,  sizstrng):
 
     subprocess.call('wmctrl',  '-i',  '-r',  winhxid, '-e',  sizstrng)
 
-def mastr1(screenx,  mastery,  masterx,  subx,  suby):
     
-    mstr = '0,10,10,'+masterx+','+mastery
-    sub = '0,'+(masterx + 10)+',10,'+subx+','+mastery
-    
-    return mstr,  sub
+##this is being called and working
+##returns int 
 
-def mastr2(screenx,  mastery,  masterx,  subx,  suby):
-    
-    mstr = '0,10,10,'+masterx+','+mastery
-    sub1 = '0,'+(masterx + 10)+',10,'+subx+','+suby
-    sub2 = '0,'+(masterx + 10)+',10,'+subx+','+(mastery - suby) 
-    
-    return mstr,  sub1,  sub2
-    
-def tilemeasure(screenx,  screeny,  pnlhght):
+def tilemeasure(strngxdim,  strngydim,  strngpnlhght):
 
+    screeny = int(strngydim)
+    panelheight = int(strngpnlhght)
     screeny = (screeny - panelheight) - 30
     mastery = screeny + 10
-    screenx = screenx - 30  
-    masterx = screenx / 5 * 3
+    screenx = int(strngxdim)
+    screenx = screenx - 30
+    masterx = int(round(screenx / 5 * 3))
     subx = screenx - masterx
-    suby = screeny / 5 * 3
+    suby = int(round(screeny / 5 * 3))
     return screeny,  mastery,  screenx,  masterx,  subx,  suby
 
-
-def execmastr1(deskid, windodb):
     
-    screenqry = windodb.Query()
-    mstr = screenqry.search((windodb.dsktp == deskid) & (windodb.role == 'master'))
-    sub1 = screenqry.search((windodb.dsktp == deskid) & (windodb.role == 'sub1'))
-    mstrs = mastr1(screenx,  mastery,  masterx,  subx,  suby).mstr
-    subs = mastr1(screenx,  mastery,  masterx,  subx,  suby).sub
-    move(mstr.hexid, mstrs)
-    move(sub1.hexid, subs)
-    
-def execmastr2(deskid):
-    
-    screenqry = tinydb.Query()
-    mstr = windodb.search((dsktp == deskid) & (role == 'master'))
-    sub1 = windodb.search((dsktp == deskid) & (role == 'sub1'))
-    sub2 = windodb.search((dsktp == deskid) & (role == 'sub2'))
-    mstrs = mastr2(screenx,  mastery,  masterx,  subx,  suby).mstr
-    sub1s = mastr2(screenx,  mastery,  masterx,  subx,  suby).sub1
-    sub2s = mastr2(screenx,  mastery,  masterx,  subx,  suby).sub2
-    move(mstr.hexid, mstrs)
-    move(sub1.hexid, sub1s)
-    move(sub2.hexid,  sub2s)
-    
-
-windodb = buildwindb()
+root = tk.Tk()
+root.bind_all('<Key>', keypress)
+# don't show the tk window
+root.withdraw()
+root.mainloop()
 while True:
+    windodb = buildwindb()
     deskid,  screenx, screeny = getscreen()
-    panelquery = Tinydb.Query()
-    panel = panelquery.desktop.search((windodb.dsktp == -1))
-    panelheight = panel.height
-    tilemeasure(screenx, screeny, panelheight)
-    root = tk.Tk()
-    root.bind_all('<Key>', keypress)
-    # don't show the tk window
-    root.withdraw()
-    root.mainloop()
-    
+    panelquery = tinydb.Query()
+    panel = windodb.get(panelquery.desktop == '-1')
+    panelheight = panel['height']
+    mastery, screeny, screenx,  masterx,  subx,  suby = tilemeasure(screenx, screeny, panelheight)
+    print(screeny,  mastery,  screenx,  masterx,  subx,  suby)
